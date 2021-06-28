bdbe86b3bca69b43685ce9908f4a7790deb6a5dd
victorvalim <victor.valim.c@gmail.com>
2021-06-28 20:44:04 -0300

    Criacao do componente GlobalStyle e das configuracao padrao para todo o css da aplicacao referente a fonte e box-sizing

---
+++ src/pages/home/components/globalStyle/index.jsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+
+  * {
+    @import url('https://fonts.googleapis.com/css2?family=Courier+Prime&display=swap');
+    font-family: 'Courier Prime' , 'Courier New' , monospace;
+    box-sizing: border-box
+
+  }
+`;
+
+export default GlobalStyle;
